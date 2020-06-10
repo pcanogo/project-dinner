7d9da68784e6bf7eaa1cede9ccd5fceaa250158c
David Medina <david.medina@sigasi.com>
2020-06-10 10:04:55 +0200

    Adding lifetime for the bullets

--- Project Dinner/Assets/Bullet.cs
+++ Project Dinner/Assets/Bullet.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
 
+
+    /// Bullet life in seconds
+    private static readonly int BULLET_LIFE = 1; 
+
     public static readonly float BULLET_SPEED = 40f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(WaitToDieCoroutine());
     }
 
     // Update is called once per frame
     void Update() 
     {
         transform.position += transform.forward * BULLET_SPEED * Time.deltaTime;
+    }
+
+    private IEnumerator WaitToDieCoroutine() {
+        yield return new WaitForSeconds(BULLET_LIFE);
+        Destroy(this.gameObject);
     }
 }
